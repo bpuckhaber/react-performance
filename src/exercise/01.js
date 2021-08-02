4b67e4112c211e4a3ebdb1d811fd9c82b00cd312
Ben Puckhaber <ben.puckhaber@gmail.com>
2021-08-02 05:48:26 -0500

    exercise.01.extra-1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,7 +2,8 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-const Globe = React.lazy(() => import('../globe'))
+const loadGlobe = () => import('../globe')
+const Globe = React.lazy(loadGlobe)
 
 function App() {
   const [showGlobe, setShowGlobe] = React.useState(false)
@@ -18,7 +19,11 @@ function App() {
         padding: '2rem',
       }}
     >
-      <label style={{marginBottom: '1rem'}}>
+      <label
+        style={{marginBottom: '1rem'}}
+        onMouseOver={loadGlobe}
+        onFocus={loadGlobe}
+      >
         <input
           type="checkbox"
           checked={showGlobe}
